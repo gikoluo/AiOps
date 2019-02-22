6f83bd5d7498d250e6579c1ad198f7c911127ca4
Luo Chunhui <wo@luochunhui.com>
2019-02-22 11:48:15 +0800

    add bin

---
+++ bin/connect.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+LDAP_SERVER="192.168.30.102"
+JENKINS_SERVER="192.168.30.200"
+
+t=`echo $1 | awk '{print toupper($0)}'`
+target="${t}_SERVER"
+eval target=\$$target
+echo "connecting to $1 $target"
+ssh -i lijidong.key "root@$target"
