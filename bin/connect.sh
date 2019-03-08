78ae24b9a8ad184061326d8a2fcab23dcb245a32
luochunhui@greenlandfinancial.com <luochunhui@greenlandfinancial.com>
2019-03-08 16:12:50 +0800

    connect

--- bin/connect.sh
+++ bin/connect.sh
@@ -3,6 +3,9 @@
 
 LDAP_SERVER="192.168.30.102"
 JENKINS_SERVER="192.168.30.200"
+GITLAB_SERVER="192.168.30.12"
+TARGET_SERVER="192.168.10.50"
+
 
 t=`echo $1 | awk '{print toupper($0)}'`
 target="${t}_SERVER"
